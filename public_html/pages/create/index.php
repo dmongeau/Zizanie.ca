eee70a8f364d2355339fbe8839036fcfcdcaa8fc
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2011-04-17 20:25:04 -0400

    Create site form

--- public_html/pages/create/index.php
+++ public_html/pages/create/index.php
@@ -1,15 +1,53 @@
 <?php
 
-?><h1>Créer un site</h1>
-
-<form action="?" method="post">
-
-<p><label>Titre :</label><br/>
-<input type="text" name="title" /></p>
-
-<p><label>Description :</label><br/>
-<textarea name="content"></textarea></p>
-
-<p><button type="submit">Ajouter</button></p>
-
-</form>
+?>
+<div class="left">
+    <h1>Créer un site</h1>
+    
+    <div class="spacer-small"></div>
+    
+    <form action="?" method="post">
+        
+        <div class="field">
+            <label>Choisissez une adresse :</label>
+            <div class="input">http://<input type="text" name="title" class="text address" maxlength="20" />.zizanie.ca</div>
+        </div>
+        
+        <div class="hr"></div>
+        
+        <div class="field">
+            <label>Titre :</label>
+            <div class="input"><input type="text" name="title" class="text title" /></div>
+        </div>
+        
+        <div class="field">
+            <label>Description :</label>
+            <div class="input"><textarea name="content"></textarea></div>
+        </div>
+        
+        <div class="hr"></div>
+        
+        <h4><a href="#" class="advanced">Options avancées</a></h4>
+        
+        <div class="advanced" style="display:none;">
+            <div class="field">
+                <label>Compte Google Analytics :</label>
+                <div class="input">
+                    <input type="text" name="analytics" class="text" style="width:200px;" />
+                    <span class="note">ex: UA-12345678-1</span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="hr"></div>
+        
+        <div class="field">
+            <label>Vérification anti-robot :</label>
+            <div class="input"><input type="text" name="title" class="text title" /></div>
+        </div>
+        
+        <div class="spacer"></div>
+        <p><button type="submit">Créer</button></p>
+    
+    </form>
+</div>
--- public_html/pages/home.php
+++ public_html/pages/home.php
@@ -1,5 +1,8 @@
 
 <div class="left">
+    
+    <div class="spacer-small"></div>
+    
 	<p class="mission"><strong>Zizanie</strong> est un gentil outil qui vous permet de créer des micro-sites axés sur la discussion et l'échange d'idées en toute liberté.</p>
     
     <div class="spacer"></div>
@@ -7,9 +10,9 @@
     <h4>Voici quelques exemples:</h4>
     <div class="spacer-small"></div>
     <ul class="point">
-    	<li>Des idées pour Fabienne Larouche</li>
-    	<li>Chara - Fuck you NHL</li>
-    	<li>Richard Martineau est contre...</li>
+    	<li><a href="http://fabiennelarouche.zizanie.ca">Des idées pour Fabienne Larouche</a></li>
+    	<li><a href="http://chara.zizanie.ca">Chara - Fuck you NHL</a></li>
+    	<li><a href="http://richardmartineau.zizanie.ca">Richard Martineau est contre...</a></li>
     </ul>
     
     <div class="spacer"></div>
